a32222ae8651db0907e3f91823aea00fad57c6b7
Benjamin Heflin and Rony Nasr <rony@>
2016-06-07 09:45:00 -0700

    Added front and backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-/*This file is for your custom js.  All yours*/
+//Back End
+var countUp = function(target,increment){
+  var resultArray = [];
+  for (var counter = increment ; counter <= target ; counter += increment){
+    resultArray.push(counter);
+  }
+  return resultArray;
+}
+
+
+
+// Front End
+$(function() {
+  $("form#input").submit(function(event) {
+    event.preventDefault();
+    //input
+    var target = parseInt($("#target").val());
+    var increment = parseInt($("#increment").val());
+    //output
+    $("#output").text(countUp(target,increment));
+  });
+});
